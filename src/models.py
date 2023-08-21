345f8032917521c52c5cb5b0cf57da07edb700b8
Bianca Szecowka <bianca.isl@gmail.com>
2023-08-21 02:52:35 +0000

    version1

--- src/models.py
+++ src/models.py
@@ -5,28 +5,69 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
 Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullable=False)
+    birth_year = Column(Date, nullable=False)
+    eye_color = Column(String(10))
+    homeworld = Column(String(50), ForeignKey(Planet.name))
+    created = Column(String(50))
+    edited = Column(String(50))
+    gender = Column(String(20))
+    hair_color = Column(String(20))
+    height = Column(Integer)
+    mass = Column(Integer)
+    skin_color = Column(String(20))
+,   url = Column(String(100))
 
     def to_dict(self):
         return {}
 
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    climate = Column(String(50))
+    created = Column(String(50))
+    edited = Column(String(50))
+    diameter = Column(Integer)
+    gravity = Column(String(50))
+    orbital_period = Column(Integer)
+    population = Column(Integer)
+    residents = Column(String) 
+    rotation_period = Column(Integer)
+    surface_water = Column(Integer)
+    terrain = Column(String(50))
+    url = Column(String(100))
+
+    def to_dict(self):
+        return {}
+
+    class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    phoneNumber = Column(String, unique=True, nullable=False)
+
+    def to_dict(self):
+        return {}
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey(User.id))
+    name = Column(String(50))
+    planet_name = Column(String(50), ForeignKey(Planet.name))
+    person_name = Column(String(50), ForeignKey(Person.name))
+    
+    def to_dict(self):
+        return {}
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
